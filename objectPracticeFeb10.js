996e9cc934b1220aa1406e239ba215b0eb8499f4
Carter Frerichs <carter@carters-MBP.attlocal.net>
2020-02-10 11:57:09 -0600

    Homework for February 10

---
+++ objectPracticeFeb10.js
@@ -0,0 +1,21 @@
+const car = {
+  size : "too small",
+  inside : "stinky",
+  steeringWheel : "flys off",
+  make : "Kia",
+  model : "Forte",
+  six : 9,
+  feel : "fine",
+  hair : "don't care",
+  national : "anthem",
+  shred : true
+};
+
+car.jimi = "Future";
+
+car.steeringWheel = "I'm toast!";
+
+let shrimp = "size";
+
+car[shrimp] = "really big";
+
